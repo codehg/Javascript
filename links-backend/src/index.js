de6b22c2f8e21ae34fde5a081893b48171cb46a3
codehg <hugoaraliveira@gmail.com>
2020-06-30 17:08:09 -0300

    Inicia projeto

---
+++ links-backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const authController = require('./controllers/auth');
+
+const app = express();
+
+app.use('/auth', authController);
+
+app.get('/', (req, res) => {
+    return res.json('Api running...');
+});
+
+app.listen(3001, () => {
+    console.log('Listening on port 3001...');
+});
